46d1b33cb128ff74adb6ce1e9b98e13e9ea90745
Анна Ковальчук <144364193+anetkovalchuk@users.noreply.github.com>
2024-09-19 03:19:35 +0300

    Update HW1.C

--- HW1.C
+++ HW1.C
@@ -11,3 +11,32 @@ int main() {
 
     return 0;
 }
+
+
+
+task2
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+int main() {
+    int a, b, c, d, e;
+    float a1, b1, c1, d1, e1;
+    //y[5] = {3.0, 1.0, 5.0, 4.0, 2.1};
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    e = 5;
+    a1 = 3.0;
+    b1 = 1.0;
+    c1 = 5.0;
+    d1 = 4.0;
+    e1 = 2.1;
+
+
+    printf("x  |   %d   |   %d   |   %d   |   %d   |   %d   |\n", a, b, c, d, e);
+    printf("---|-------|-------|-------|-------|-------|\n");
+    printf("y  |  %.1f  |  %.1f  |  %.1f  |  %.1f  |  %.1f  |\n", a1, b1, c1, d1 , e1);
+
+    return 0;
+}
